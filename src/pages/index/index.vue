252ae8afd6d9e54df67067761dc7f9e4db5f0186
robin901118 <303278055@qq.com>
2019-06-24 15:34:57 +0800

    新增授权demo

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -12,19 +12,44 @@
 			</myInput>
 			<view class="nullFloor"></view>
 			<myInput name="password" bindName="password" labelName="密码" :isPassword="showPsw">
-				<button
-				slot="boxRightContent"
-				class="togglePsw"
-				size="mini"
-				@click="togglePsw">
-				{{showPsw ? "隐藏" : "显示"}}
+				<button slot="boxRightContent" class="togglePsw" size="mini" @click="togglePsw">
+					{{showPsw ? "隐藏" : "显示"}}
 				</button>
 			</myInput>
 			<view class="nullFloor"></view>
 			<view class="flex mainRight">
 				<button class="forgetBtn" size="mini">忘记密码？</button>
 			</view>
-			<button formType="submit" class="loginBtn">登录</button>
+			<!-- 已授权 -->
+			<template v-if="isAuthed">
+				<!-- #ifdef MP-WEIXIN -->
+				<button
+				formType="submit"
+				class="loginBtn">微信登录</button>
+				<!-- #endif -->
+				
+				<!-- #ifdef MP-ALIPAY -->
+				<button
+				formType="submit"
+				class="loginBtn">支付宝登录</button>
+				<!-- #endif -->
+			</template>
+			
+			<!-- 未授权 -->
+			<template v-else>
+				<!-- #ifdef MP-WEIXIN -->
+				<button
+				class="loginBtn"
+				@getuserinfo="getUserInfo"
+				open-type="getUserInfo">微信授权</button>
+				<!-- #endif -->
+				
+				<!-- #ifdef MP-ALIPAY -->
+				<button
+				class="loginBtn"
+				@click="getUserInfo">支付宝授权</button>
+				<!-- #endif -->
+			</template>
 		</form>
 	</view>
 </template>
@@ -36,9 +61,12 @@
 		data() {
 			return {
 				showPsw: false,
+				isAuthed:false,//是否授权过
 			}
 		},
-		components:{myInput},
+		components: {
+			myInput
+		},
 		methods: {
 			/**
 			 * 密码显示隐藏
@@ -52,7 +80,6 @@
 			 * **/
 			loginSubmit(e) {
 				const val = e.detail.value;
-				console.log(val);
 				if (val.tel && val.password) {
 					// 登录成功，跳转api列表页面
 					uni.navigateTo({
@@ -60,12 +87,76 @@
 					});
 				} else {
 					uni.showToast({
-						title: '登录失败',
-						icon: "icon",
+						title: '电话和密码不能为空',
+						icon: "none",
 						duration: 2000
 					});
 				}
+			},
+			
+
+			/**
+			 * 获取用户信息
+			 * **/
+			getUserInfo(e) {
+				//微信小程序授权
+				// #ifdef MP-WEIXIN
+				if(e.detail.hasOwnProperty('userInfo')){
+					uni.showToast({
+						title: '授权成功',
+						icon: "success",
+						duration: 2000
+					});
+					this.isAuthed = true;
+				}else{
+					uni.showToast({
+						title: '您拒绝了授权',
+						icon: "none",
+						duration: 2000
+					});
+				}
+				// #endif
+				
+				//支付宝小程序授权
+				// #ifdef MP-ALIPAY
+				my.getAuthCode({
+				  scopes: 'auth_user',
+				  success: (res) => {
+					my.getAuthUserInfo({
+					  success: (userInfo) => {
+						  console.log('获取授权成功');
+						  // console.log('userInfo',userInfo);
+						  this.isAuthed = true;
+					  }
+					});
+				  },
+				});
+				// #endif
 			}
+		},
+		mounted() {
+			// 微信获取授权列表
+			// #ifdef MP-WEIXIN
+			uni.getSetting({
+				success: res => {
+					this.isAuthed = res.authSetting['scope.userInfo'];
+				},
+				fail:() => {
+					console.log('失败')
+				}
+			});
+			// #endif
+			
+			//支付宝小程序
+			// #ifdef MP-ALIPAY
+			my.getSetting({
+				success:res=>{
+					console.log('授权列表',res.authSetting['userInfo']);
+					this.isAuthed = res.authSetting['userInfo']
+				}
+			})
+			// #endif
+
 		}
 	}
 </script>
